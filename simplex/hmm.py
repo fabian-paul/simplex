e37b4978b61f4d953ff678462e2fd5f3c26f4ddd
Fabian Paul <paul@mpikg.mpg.de>
2018-03-04 14:51:28 +0100

    w.i.p.

--- simplex/hmm.py
+++ simplex/hmm.py
@@ -1,48 +1,135 @@
 # TODO: think about HMM with multiple emissions
+import numpy as np
+import msmtools
 
 def softmax(x):
     return np.exp(x) / np.exp(x).sum(axis=1)[:, np.newaxis]
 
 class NullLogger(object):
-    def log(self, *+kwargs)
+    def log(self, **kwargs):
         pass
+
+class MSM(object):
+    # wrap Pyemma MSM or msmtools MSM
+    def __init__(self, n_states):
+        self.n_states = n_states
+        # gap: its > tau and large relative gap...
+        # TODO: automatic identification of spectral gap with RMA?
+
+    def estimate(self, observations, lag):
+        C = msmtools.estimation.count_matrix(observations, lag=lag)
+
+        # compute memberships
+        lcs = msmtools.estimation.largest_connected_set(C, directed=False)
+        T = np.zeros(C.shape)
+        T[lcs, lcs[:, np.newaxis]] = msmtools.estimation.tmatrix(C[lcs, :][:, lcs], revsersible=False).todense()
+
+        C_back = msmtools.estimation.count_matrix([o[::-1] for o in observations], lag=lag)
+        lcs_back = msmtools.estimation.largest_connected_set(C_back, direced=False)
+        T_back = np.zeros(C.shape)
+        T_back[lcs_back, lcs_back[:, np.newaxis]] = msmtools.estimation.tmatrix(C_back[lcs_back, :][:, lcs_back], reversible=False).todense()
+        
+        T_forward_backward = T.dot(T_back)
+        lcs_forward_backward = msmtools.estimation.largest_connected_set(T_forward_backward, directed=True)
+        chi = np.zeros((C.shape[0], self.n_states))
+        chi[lcs_forward_backward, :] = msmtools.analysis.pcca(T_forward_backward[lcs_forward_backward, :][:, lcs_forward_backward], self.n_states)
+
+        # compute pi
+        # TODO: compute individual pi for all the disconnected blocks and weight by frequency???
+        lcs_pi = msmtools.estimation.largest_connected_set(C, directed=True)
+        T_pi = np.zeros(C.shape)
+        T_pi[lcs, lcs[:, np.newaxis]] = msmtools.estimation.tmatrix(C[lcs, :][:, lcs]).todense()
+        pi = np.zeros(C.shape[0])
+        pi[lcs_pi] = msmtools.analysis.statdist(T_pi[lcs_pi, :][:, lcs_pi])
+        self.PI = np.diag(pi)
+        # TODO: we don't really don't need any connectivity when doing PCCA...
+        # can we work with an ill-defined pi? 
+        # ideally we should just do pcca on all the disconnected parts... then there is always a defined pi
+        
+        # membership will be defined on the full observed space (but might be zero for some states in the observed space)
+        self.b = chi.dot(PI)
+        # coarse-grained T matrix
+        self.T = np.linag.inv(chi.dot(PI).dot(chi.T)).dot(chi.dot(T).dot(chi.T))
+
+    @property
+    def b(self):
+        return self.b
+
+    @property
+    def p0(self, use_all=False):
+        if not use_all:
+            # use initial memberships
+            p0 = np.array([ chi[o[0], :] for o in observations ]).mean(axis=0)
+            return p0
+        else:
+            # use empirical counts
+            vset, counts = np.unique(np.concatenate(observations), return_counts=True)
+            p0 = np.zeros()
+            p0[vset] = counts
+            return p0 / p0.sum()
+
+    @property
+    def T(self):
+        'coarse-grained transition matrix'
+        return self.T
 
 class DiscreteEmissionModel(object):
     def __init__(self):
-       # TODO: multiple trajectories ...
-       pass
+        pass
 
-    def estimate(self, observations, metastable_memberships):
-        self.observations = observations # TODO: optimize
-        self._unique_observations = np.unique([np.unique(o) for o in observations])
-        self._b = np.zeros(np.max(self._unique_observations)+1)
+    def initialize(self, model, observations):
+        self.observations = observations
+        assert isinstance(model, MSM)
+        self._b = model.b
+
+    # TODO: make estimate a function
+    def estimate(self, metastable_memberships):
+        'estimate b_i(k) from the fixed observed space trajectories and the metastable memberships'
+        self._unique_observations = np.unique([np.unique(o) for o in self.observations])
+        self._b = np.zeros(np.max(self._unique_observations) + 1)
         gamma = metastable_memberships
-        norm = gamma.sum(axis=0)
-        for o,g in zip(observations, gamma):
+        norm = gamma.sum(axis=0) # sum over all frames (time steps)
+        for o, g in zip(observations, gamma):
             for i in self._unique_observations:
                 self._b[i, :] += (g[o==i, :]).sum(axis=0) / norm
+        # TODO: rewrite such that we return a model; don't change this object's state!
 
     def emission_probability(self): # TODO better name
+        'return [ b_i(O_(t)) ]_t,i'
         # TODO: make b trajectory-depdent (list)
-        return [self._b[o] for o in self.observations]
-        
+        return [ self._b[o] for o in self.observations ]
+
+    # alternative implementation of what???
     def b_alt(self):
         observations = dense(self.observations)
         # TODO: check index order!
-        b = (metastable_memberships[:, np.newaxis, :]*observations[:, :, np.newaxis]).sum(axis=0) / metastable_memberships.sum(axis=0)
-        return observations.dot(b)
+        #b = (metastable_memberships[:, np.newaxis, :]*observations[:, :, np.newaxis]).sum(axis=0) / metastable_memberships.sum(axis=0)
+        #return observations.dot(b)
         return observations.dot((metastable_memberships[:, np.newaxis, :]*observations[:, :, np.newaxis]).sum(axis=0) / metastable_memberships.sum(axis=0))
         
 
 class SimplexCoreMSM(object):
     # simplex and TBA MSM
-    pass
+    def estimate(self, observations, lag):
+        from . import milestoning_count_matrix, find_vertices_inner_simplex, core_assignments, memberships
+        vertices = find_vertices_inner_simplex(observations)
+        ctrajs = core_assignments(vertices, observations)
+        C = milestoning_count_matrix(ctrajs)
+        T = C / C.sum(axis=1)[:, np.newaxis]
+        self.T = T
+        self.vertices = vertices
+        return SimplexCoreMSMModel(self.T, self.vertices, ctrajs)
+
+    @propterty
+    def T(self):
+        return self.T
 
 class SimplexEmissionModel(object):
     def __init__(self):
         self.W = None
         
     def initialize(self, model):
+        assert isinstance(model, SimplexCoreMSM)
         # get W from simplex
         self.W = model.vertices # TODO: take inverse! somehow, see lyx
         self.observations = model.tics # TODO: name?
@@ -82,54 +169,45 @@ class SimplexEmissionModel(object):
     def vertices(self):
         return self.W # TODO: correct me!
 
+# to the wiring on the api function level
+def hmm():
+    pass
 
-class MSM(object):
-    # wrap Pyemma MSM or msmtools MSM
-    def __init__(self):
-        pass
-    
-    def estimate(self, observations):
-        C = msmtools.estimation.count_matrix(observations)
-        self.T = msmtools.estimation.tmatrix(C).todense()
-
-    @property
-    def b(self):
-        pass
-
-    @property
-    def p0(self):
-        pass
-
-    @property
-    def a(self):
-        pass
-        # coarse-grained? matrix
-        
-    T = a
 
 class HMM(object):
-    def __init__(self, n_states, initial_model=MSM(), emission_model=DiscreteEmissionModel(), observations, logger=ZeroLogger()):
+    def __init__(self, observations, lag, initial_model=MSM(n_states=None), emission_model=DiscreteEmissionModel(), logger=NullLogger(), max_iter=1000000, tol=1.E-6):
         self.emission_model = emission_model
+        self.initial_model = initial_model
+        # can we also pass an estimated model?
+        if not initial_model.estimated:
+            initial_model.estimate(observations=observations, lag=lag)
         T = len(observations)
         alpha = np.zeros((T, n_states))
         beta = np.zeros((T, n_states))
         self.logger = logger
+        self.emission_model.intialize(initial_model, observations)
+        self.max_iter = max_iter
+        self.tol = tol
 
     def estimate(self):
         # initialization step
         # pi = counts or initial memberships?? since we use gamma later...
-        self.emission_model.intialize(initial_model) # or so variant of parameter update?
+        #self.emission_model.intialize(initial_model) # or so variant of parameter update?
 
         # TODO: make alpha and beta trajectory-dependent -> list of nparray
         # TODO: think about pi (keep list of pi or make one global one?) How is Frank doing it? local pi makes more sense
-        while True:
+        
+        pi = self.initial_model.p0  # attention: pi means p0 here!
+        a = self.intial_model.T
+        last_L = float('inf')
+        for iteration in range(self.max_iter):
             # expectation step
             b = self.emission_model.emission_probability()
         
             alpha[0, :] = b[0, :]*pi
             for t in range(1, T): # TODO: scaling
                 alpha[t, :] =  alpha[t-1, :].dot(self.a)*b[t, :]
-                beta[-1, :] = 1
+            beta[-1, :] = 1
             for t in range(T-2, 0, -1):
                 b[t, :] = a.dot(b[t+1, :]*beta[t+1, :])
 
@@ -140,11 +218,22 @@ class HMM(object):
              xi = temp / temp.sum(axis=2).sum(axis=1)[:, np.newaxis, np.newaxis]
 
              # compute likelihood
-             L = alpha[-1, :].sum() # P(O|lambda) in the paper # TODO: make trajectory-dependent
+             L = alpha[-1, :].sum() # P(O|lambda) in the paper # TODO: make trajectory-dependent (see one of the appendices in the paper)
+             delta_L = abs(last_L - L) 
 
              # maximization step
              self.emission_model.estimate(alpha*beta)
+             # continue with which memberships???? Read paper again!
+             
+             # update pi (p0 and the hidden transition matrix)
              pi = gamma[0, :]
              a = xi.sum(axis=0) / gamma.sum(axis=0) # CHECK
+             
+             if delta_L < self.tol:
+                 break
+
+        self.a = a
+        self.pi = pi
+        self.gamma = gamma
 
         self.logger.log(self)
--- simplex/test/test_hmm.py
+++ simplex/test/test_hmm.py
@@ -6,10 +6,28 @@ class TestHmm(unittest.TestCase):
     def setup(cls):
         pass
     
-    def test_hmm(self):
-        N = 10
-        ref_vertices = np.random.randn((N+1, N))
-        # sample from 
+    #def test_hmm(self):
+    #    N = 10
+    #    ref_vertices = np.random.randn((N+1, N)) #  these define the "random transformation"; make sure non-degenerate
+    #    # sample from 
+        # u are uniform random numbers
+        # c is the globsal steepness of the core 
+        # m_self = np.log((np.exp(-c) + 1)*np.exp(u*c) - 1)/(2*c) + 0.5 #  this leads to very fuzzy states
+        # m_other = np.random.rand(N)
+        # m_other = m_other / (m_other.sum() - m_self)
+        # m[i] = m_self
+        # m[other] = m_other
+        # now apply random non-singular linear transform (fixed for all the data) ideally use orthogonal transform
+
+        
+        
+    
+    def test_discrete_hmm(self):
+        # generate a small transtion matrix + generate a trajectory
+        # map this to a larger space (preferentially 1-D)
+        
+        # 
+        
 
 if __name__ == '__main__':
     unitest.main()
